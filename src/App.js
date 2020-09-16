a267bed9a11a678e5f638d5f682d8c8c9c86b5bf
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2020-09-16 11:21:11 +0100

    Initial CRA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
+import ApolloClient from 'apollo-boost';
 import React from 'react';
-import logo from './logo.svg';
+import { ApolloProvider } from 'react-apollo';
+
+// import Questions from "./components/questions"
 import './App.css';
 
-function App() {
+const client = new ApolloClient({
+  uri: "https://api.8base.com/ckf57jmd9000208l10o1t0id7"
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div className="App">
+        {/* <Questions /> */}
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App;
