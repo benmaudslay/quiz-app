a65ff27971a1bf417d55b9eb3e19fb0d7417b76d
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2020-09-22 11:46:25 +0100

    Updates

---
+++ precache-manifest.526411dc8d3054ffeb4496ff571b084d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26d6028a39d6197bd3f41147df070b4",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "3bd8e0a9ea2400435c28",
+    "url": "/quiz-app/static/css/2.bee261c4.chunk.css"
+  },
+  {
+    "revision": "c1af4686c5cb6de5297a",
+    "url": "/quiz-app/static/css/main.3361463c.chunk.css"
+  },
+  {
+    "revision": "3bd8e0a9ea2400435c28",
+    "url": "/quiz-app/static/js/2.c2e087f0.chunk.js"
+  },
+  {
+    "revision": "a487d88dd5d1a46cbf14d9096cd48421",
+    "url": "/quiz-app/static/js/2.c2e087f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1af4686c5cb6de5297a",
+    "url": "/quiz-app/static/js/main.069b323a.chunk.js"
+  },
+  {
+    "revision": "a8ec7b82942a266765d4",
+    "url": "/quiz-app/static/js/runtime-main.99adc817.js"
+  },
+  {
+    "revision": "e71bd1500a430c0bf68061cd00a5df72",
+    "url": "/quiz-app/static/media/add.e71bd150.svg"
+  },
+  {
+    "revision": "ca028cb46141fdaf8c7e373ca32b7c92",
+    "url": "/quiz-app/static/media/cancel.ca028cb4.svg"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/quiz-app/static/media/got.3af178ec.ttf"
+  }
+]);
